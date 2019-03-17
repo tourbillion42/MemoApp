07dd492313628cae9b3c6fe0ed2b14ca71283260
tourbillion42 <tourbillion42@gmail.com>
2019-03-17 23:17:43 +0900

    add MemoReadVC, MemoListVC

---
+++ MemoApp/MemoReadVC.swift
@@ -0,0 +1,30 @@
+//
+//  MemoReadVC.swift
+//  MemoApp
+//
+//  Created by Hwang on 17/03/2019.
+//  Copyright © 2019 Hwang. All rights reserved.
+//
+
+import UIKit
+
+class MemoReadVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
