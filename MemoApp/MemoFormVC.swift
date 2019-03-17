3b08e32e8f793a0de7685a0d671f0c5b0ec15e99
tourbillion42 <tourbillion42@gmail.com>
2019-03-18 05:50:01 +0900

    add imagePickerController and textViewDidChange function

--- MemoApp/MemoFormVC.swift
+++ MemoApp/MemoFormVC.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 
-class MemoFormVC: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MemoFormVC: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate {
     
     var subject : String!
     
     @IBOutlet var contents: UITextView!
     @IBOutlet var preview: UIImageView!
+    
+    override func viewDidLoad() {
+        self.contents.delegate = self
+    }
     
     @IBAction func save(_ sender: Any) {
         
@@ -21,6 +25,27 @@ class MemoFormVC: UIViewController,UIImagePickerControllerDelegate, UINavigation
     
     @IBAction func pick(_ sender: Any) {
         
+        let picker = UIImagePickerController()
         
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        self.present(picker, animated: false, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        self.preview.image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        
+        picker.dismiss(animated: false, completion: nil)
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        
+        let contents = textView.text as NSString
+        let length = ( (contents.length > 15) ? 15 : contents.length )
+        self.subject = contents.substring(with: NSRange(location: 0, length: length))
+        
+        self.navigationItem.title = subject
     }
 }
