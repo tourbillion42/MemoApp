7c748aa5f0a09a1071c72e7ded18b01f6352129d
tourbillion42 <tourbillion42@gmail.com>
2019-03-18 07:04:44 +0900

    add MemoReadVc function

--- MemoApp/MemoReadVC.swift
+++ MemoApp/MemoReadVC.swift
@@ -9,22 +9,24 @@
 import UIKit
 
 class MemoReadVC: UIViewController {
-
+    
+    var param : MemoData?
+    
+    @IBOutlet var subject: UILabel!
+    @IBOutlet var contents: UILabel!
+    @IBOutlet var img: UIImageView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.subject.text = param?.title
+        self.contents.text = param?.contents
+        self.img.image = param?.image
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd일 HH:mm분에 작성됨"
+        let dateString = formatter.string(from: (param?.regdate)!)
+        
+        self.navigationItem.title = dateString
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
