480a6d14d4c33e4b026714d42c278f6ceba23ff3
tourbillion42 <tourbillion42@gmail.com>
2019-03-17 23:17:09 +0900

    add  MemoCell

---
+++ MemoApp/MemoCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemoCell.swift
+//  MemoApp
+//
+//  Created by Hwang on 17/03/2019.
+//  Copyright © 2019 Hwang. All rights reserved.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
